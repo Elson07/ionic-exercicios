236fec2e87064a4816540174392c943dd4f1a3a9
Elson07 <elsonnunes07@gmail.com>
2023-06-08 11:56:06 -0300

    Exercicío Data Binding no Angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,18 @@ export class HomePage {
 
   constructor() {}
 
+  public titulo: String = "Meu primerio App";
+  public imagemRandomica: String = "https://images.unsplash.com/photo-1532704384331-d96a9862ec11?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4NTYyMTcwNw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080";
+  public imagemLocal: String = "../assets/icone-celular.png"
+
+  public atualiza(): void {
+    this.titulo = "Texto alterado";
+  } 
+
+
+  public acao(): void {
+    this.titulo = "Botão clicado";
+  } 
+
+
 }
