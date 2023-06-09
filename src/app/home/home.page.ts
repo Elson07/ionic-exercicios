9e3905d94cd0d1e29f2ba0935cb64564febfd4ed
Elson07 <elsonnunes07@gmail.com>
2023-06-09 11:56:10 -0300

    Exercicío de navegação e modelo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//Biblioteta para navegação
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  implements OnInit {
 
-  constructor() {}
+  //O NavController é um objeto da biblioteca de navegação que possui algun metodos para tal
+  constructor(private navegacao: NavController) {}
 
-  public titulo: String = "Meu primerio App";
-  public imagemRandomica: String = "https://images.unsplash.com/photo-1532704384331-d96a9862ec11?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4NTYyMTcwNw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080";
-  public imagemLocal: String = "../assets/icone-celular.png"
-
-  public atualiza(): void {
-    this.titulo = "Texto alterado";
-  } 
+  ngOnInit() {
+  }
 
 
-  public acao(): void {
-    this.titulo = "Botão clicado";
-  } 
+  abrirBotoes() {
+    this.navegacao.navigateForward('botoes')
+  }
 
+
+  abrirLista() {
+    this.navegacao.navigateForward('lista')
+  }
+
+
+  //Resebe o valor passado pelo input do ngModel
+  public pesquisa: String = "";
+  public resultado: String = "";
+
+
+  recuperar() {
+    this.resultado = this.pesquisa;
+  }
 
 }
